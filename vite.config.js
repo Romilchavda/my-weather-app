caee1126593df8ff55118ce822bd632b0ff43cad
Romil Chavda <89027751+Romilchavda@users.noreply.github.com>
2026-04-02 19:03:13 +0530

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './my-weather-app', // <--- Ye line zaroor add karein
+  base: '/my-weather-app/',  // <--- DHAYAN SE: Apne repo ka naam '/' ke beech mein likhein
 })
